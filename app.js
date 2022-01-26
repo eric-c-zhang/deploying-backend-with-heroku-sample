6a99929f4157d273845126c0704b399e275d5976
eric-c-zhang <90730607+eric-c-zhang@users.noreply.github.com>
2022-01-26 03:09:38 -0500

    finally

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const fetch = require('node-fetch'); // import node-fetch (enables the fetch API
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
 var fs = require("fs");
 var text = fs.readFileSync("./covmat.json");
-var text2 = fs.readFileSync("./updatematrix.json")
+var text2 = "test2"
+var text2 = fs.readFileSync("./matrixnew.json")
 
 app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
   fetch('https://www.boredapi.com/api/activity') // fetch activity from bored API - https://www.boredapi.com/about
---
+++ update-matrix.js
@@ -0,0 +1,17 @@
+var assets = ['XSP.TO','XIC.TO','XIN.TO','XBB.TO','CAR-UN.TO'];
+var covmatMain = [];
+const getetfdata = require('./resources/js/getetfdata.js');
+const fs = require('fs');
+
+getetfdata(assets)
+.then(matrix=>{
+    covmatMain=matrix;
+    console.log(covmatMain);
+    fs.writeFile('./matrixnew.json', JSON.stringify(covmatMain), 'utf8', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+    });
+})
